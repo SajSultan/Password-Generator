59e914ea85664592b90e08bfad8eb1d0961d3914
SajSultan <ksa.sultan1979@yahoo.com>
2023-01-28 23:21:44 +0000

    created working first attempt of random passwrord array.

--- script.js
+++ script.js
@@ -88,45 +88,70 @@ var upperCasedCharacters = [
   "Z",
 ];
 
-let numberOfChars;
-let randomArray = [];
-let userChoiceArr = [];
+let numberOfChars; 
+
+
 
 let lowerConfirm;
 let upperConfirm;
 let specialConfirm;
 let numericConfirm;
  
-// 1. Prompt user for password character length
-passwordCharLength = (prompt(
-  "Enter number of characters for password generation from range 10 to 64 to begin."
-));
-numberOfChars = numberOfChars + passwordCharLength;
 
 
-if ((passwordCharLength) >= 10 && passwordCharLength <= 64) {
-  lowerConfirm = confirm("would you like to use lowercase characters?");
 
-  if (confirm("would you like to use upper case characters?")) {
-    userChoiceArr = userChoiceArr.concat(upperCasedCharacters);
+// Function to prompt user for password options
+function getPasswordOptions() {
+  // 1. make variable for user chosen criteria empty array.
+  let userChoiceArr =[];
+  //2. Prompt user for password character length.
+  passwordCharLength = (prompt(
+    "Enter number of characters for password generation from range 10 to 64 to begin."
+  ));
+  // 3. Create if statement for matching and choosing criteria to push to new array.
+  if ((passwordCharLength) >= 10 && passwordCharLength <= 64) {
+    confirm("would you like to use lowercase characters?");
+    userChoiceArr = userChoiceArr.concat(lowerCasedCharacters);
+  // 3.a. concat user choice of arrays to make a new array
+    if (confirm("would you like to use upper case characters?")) {
+      userChoiceArr = userChoiceArr.concat(upperCasedCharacters);
+    } //3.b.  concat user choice of arrays to make a new array
+    if (confirm("would you like to use special characters?")) {
+      userChoiceArr = userChoiceArr.concat(specialCharacters);
+    } //3.c.  concat user choice of arrays to make a new array
+    if (confirm("would you like to use numeric characters?")) {
+      userChoiceArr = userChoiceArr.concat(numericCharacters);
+      // 3.d. concat user choice of arrays to make a new array
+    } else {
+      (confirm("Please choose a number and characters to use for your password")) 
+        getPasswordOptions(prompt);
+        // need to fix
+      }
+    }  
   }
-  if (confirm("would you like to use special characters?")) {
-    userChoiceArr = userChoiceArr.concat(specialCharacters);
-  }
-  if (confirm("would you like to use numeric characters?")) {
-    userChoiceArr = userChoiceArr.concat(numericCharacters);
-  } 
-  console.log(userChoiceArr)
+
+
+getPasswordOptions();
+
+
+
+
+// Function for getting a random element from an array.
+function getRandom() {
+let randomsArray = [];
+for(let i = 0; i < numberOfChars.length; i++) {
+  randomsArray = Math.floor(Math.random()* userChoiceArr.length);
+  randomsArray += userChoiceArr[randomsArray]
+}
+  return randomsArray;
+  console.log(getRandom)
 
 }
 
-// Function to prompt user for password options
-function getPasswordOptions() {}
-// Function for getting a random element from an array
-function getRandom() {}
-
 // Function to generate password with user input
 function generatePassword() {}
+password = "";
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
