a52ae130de54099567ba8a1f2dd02cd0625bb20e
SajSultan <ksa.sultan1979@yahoo.com>
2023-02-24 13:46:50 +0000

    Commit for push

--- script.js
+++ script.js
@@ -89,17 +89,12 @@ var upperCasedCharacters = [
 ];
 
 let userChoiceSorted = [];
-let numberOfChars;
-let lowerConfirm;
-let upperConfirm;
-let specialConfirm;
-let numericConfirm;
-let userChoiceArr;
+let userChoiceArr = [];
+
 let passwordCharLength;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  userChoiceArr = [];
   // 1. make variable for user chosen criteria empty array.
 
   //2. Prompt user for password character length.
@@ -108,61 +103,62 @@ function getPasswordOptions() {
       "Enter number of characters for password generation from range 10 to 64 to begin."
     )
   );
-  if (isNaN(passwordCharLength)) {
-    alert(
-      "You must choose a value between 10 to 64 and a minimum of 1 of the corresponding character choices to generate a pasword?"
-    );
-    return null;
-  }
+  // if (isNaN(passwordCharLength)) {
+  //   alert(
+  //     "You must choose a value between 10 to 64 and a minimum of 1 of the corresponding character choices to generate a pasword?"
+  //   );
+
   // 3. Create if statement for matching and choosing criteria to push to new array.
 
-  if (passwordCharLength >= 10 && passwordCharLength <= 64) {
-    lowerConfirm = confirm("would you like to use lowercase characters?");
-    lowerConfirm = userChoiceArr.concat(lowerCasedCharacters);
-    upperConfirm = confirm("would you like to use uppercase characters?");
-    upperConfirm = userChoiceArr.concat(upperCasedCharacters);
-    specialConfirm = confirm("would you like to use special characters?");
-    specialConfirm = userChoiceArr.concat(specialCharacters);
-    numericConfirm = confirm("would you like to use numeric characters?");
-    numericConfirm = userChoiceArr.concat(numericCharacters);
-    
-    
-    
+  let lowerConfirm = confirm("would you like to use lowercase characters?");
+  let upperConfirm = confirm("would you like to use uppercase characters?");
+  let specialConfirm = confirm("would you like to use special characters?");
+  let numericConfirm = confirm("would you like to use numeric characters?");
+  if (lowerConfirm) {
+    userChoiceArr.push(lowerCasedCharacters);
   }
- 
- 
- console.log(userChoiceArr);
- console.log(passwordCharLength);
-  
+
+  if (upperConfirm) {
+    userChoiceArr.push(upperCasedCharacters);
+  }
+
+  if (specialConfirm) {
+    userChoiceArr.push(specialCharacters);
+  }
+
+  if (numericConfirm) {
+    userChoiceArr.push(numericCharacters);
+  }
+
+  console.log(userChoiceArr);
+  console.log(getRandom(userChoiceArr)) ;
+}
 
 
- }
 
-//   // Function for getting a random element from an array.
-//   function getRandom(passwordCharLength, userChoiceSorted){
-//   getPasswordOptions()
-//     let randomsArray = []
-//     for(let i = 0; i < passwordCharLength.length; i++) {
-//       randomsIndex = Math.floor(Math.random() * numberOfChars.length);
-//       randomsArray += userChoiceArr[randomsIndex];
-//     }
-//   //   return randomsArray;
+//Function for getting a random element from an array.
 
-//   }
-//   // getRandom()
-//   // Function to generate password with user input
-//   function generatePassword() {
-//     let password = "";
+function getRandom(array){
+ return array[Math.floor(Math.random() * array.length)]
 
-//     return getPasswordOptions();
-//   };
+
+
+}
+getRandom()
+//Function to generate password with user input
+function generatePassword() {
+  console.log("CLICKED");
+  let password = "";
+
+  return "Generated password will go here!";
+}
 
 // // Get references to the #generate element
 // var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
 // function writePassword() {
-//   // console.log("CLICKED");
+
 //   const password = generatePassword();
 //   var passwordText = document.querySelector("#password");
 
